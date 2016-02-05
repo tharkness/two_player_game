87559c80507772c616c59aec15e94a95f6bb6cf2
vagrant <vagrant@lighthouse-labs>
2016-02-05 21:31:27 +0000

    game functions

--- two_player_game.rb
+++ two_player_game.rb
@@ -21,16 +21,45 @@ def player_2_name
 end
 
 def game
-  while (@player_1[:lives] && @player_2[:lives]) > 0
-    puts "Player 1: #{rand(21)} + #{rand(21)} is equal to what?"
-    p_one_answer = gets.chomp
-      case p_one_answer
-      when p_one_answer !=~ /\d/
+  puts "Game will now start, get ready."
+  while (@player_1[:lives] > 0) && (@player_2[:lives] > 0)
+    num1 = rand(21)
+    num2 = rand(21)
+    num3 = rand(21)
+    num4 = rand(21)
+    puts "Player 1: Answer this question."
+    player_1_question = "#{num1} + #{num2}"
+    puts "#{player_1_question.to_s}"
+    player_1_answer = gets.chomp
+      if player_1_answer.to_i == num1 + num2
+        @player_1[:score] += 1
+        puts "Correct! Score is now #{@player_1[:score]}."
+      else
         @player_1[:lives] -= 1
-        puts "#{@player_1[:name]}...you done fucked up, you now have #{@player_1[:lives]}."
+        puts "Wrong! You are dead and now have #{@player_1[:lives]} lives."
+      end 
+    puts "Player 2: Answer this question."
+    player_2_question = "#{num3} + #{num4}"
+    puts "#{player_2_question.to_s}"
+    player_2_answer = gets.chomp
+      if player_2_answer.to_i == num3 + num4
+        @player_2[:score] += 1
+        puts "Correct! Score is now #{@player_2[:score]}."
+      else
+        @player_2[:lives] -= 1
+        puts "Wrong!  You are dead and now have #{@player_2[:lives]} lives."
       end
   end
 end
+
+def result
+  if @player_1[:lives] == 0
+    puts "#{@player_1[:name]} you suck, go home.  #{@player_2[:name]} you're awesome, you win."
+  else
+    puts "#{@player_2[:name]} you suck, go home.  #{@player_1[:name]} you're awesome, you win."
+  end
+end
+
 
 player_1_name
 
@@ -45,3 +74,5 @@ p_two_response = gets.chomp
 player_2_name if p_two_response == 'no'
 
 game
+
+result
